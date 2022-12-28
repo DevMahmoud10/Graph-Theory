6c0ec14e389c62a99ab8b54ca4e126866c8a0bd4
Mahmoud Ahmed <DevMahmoud10@users.noreply.github.com>
2022-12-28 15:35:16 +0200

    Create detect_cycle_in_graph.py

---
+++ detect_cycle_in_graph.py
@@ -0,0 +1,44 @@
+def has_cycle(graph):
+    def is_node_in_cycle(graph, node, visited, currently_in_stack):
+        visited.add(node)
+        currently_in_stack.add(node)
+        for n in graph[node]:
+            if n in currently_in_stack:
+                return True
+            if n in visited:
+                continue
+            is_in_cycle=is_node_in_cycle(graph, n, visited, currently_in_stack)
+            if is_in_cycle:
+                return True
+        currently_in_stack.remove(node)
+        return False
+
+    visited=set()
+    currently_in_stack=set()
+    for node in graph:
+        if node in visited:
+            continue
+        contains_cycle=is_node_in_cycle(graph, node, visited, currently_in_stack)
+        if contains_cycle:
+            return True
+    return False
+            
+graph = {
+    1: [2, 4],
+    2: [3],
+    3: [3],
+    4: [5],
+    5: [2]
+}
+res=has_cycle(graph)
+assert res==True
+
+graph = {
+    1: [2, 4],
+    2: [3],
+    3: [],
+    4: [5],
+    5: [2]
+}
+res=has_cycle(graph)
+assert res==False
